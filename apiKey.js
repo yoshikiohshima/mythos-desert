6c3bdc61228c789f19633495f1a8f2bdfefc966c
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2023-07-26 15:03:40 -0700

    change api key

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
-const apiKey = '1DpF4lnxxGr0IjKbKlOm58A9GuKbFyKaxh2PDK87x';
-const appId = 'io.croquet.mythos';
+const apiKey = '1MGcxf1GGH5OKocNt52mx7F972EpEvlat2FalePr0';
+const appId = 'io.croquet.yoshiki.mythos-desert';
 export default {apiKey, appId};
